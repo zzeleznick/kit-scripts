41b495f4233394f85d47191a31504567d41183ca
zzeleznick <zzeleznick@gmail.com>
2021-03-26 09:59:23 -0700

    add app-launcher from https://scriptkit.app/scripts/johnlindquist/app-launcher

---
+++ app-launcher.js
@@ -0,0 +1,51 @@
+// Menu: App Launcher
+// Description: Search for an app then launch it
+// Author: John Lindquist
+// Twitter: @johnlindquist
+// Shortcut: Alt+A
+
+let {fileSearch} = await kit('file')
+
+let choices = async () => {
+  let apps = await fileSearch('', {
+    onlyin: '/',
+    kind: 'application',
+  })
+
+  let prefs = await fileSearch('', {
+    onlyin: '/',
+    kind: 'preferences',
+  })
+
+  let group = (path) => (apps) =>
+    apps
+      .filter((app) => app.match(path))
+      .sort((a, b) => {
+        let aName = a.replace(/.*\//, '')
+        let bName = b.replace(/.*\//, '')
+        return aName > bName ? 1 : aName < bName ? -1 : 0
+      })
+
+  return [
+    ...group(/^\/Applications\/(?!Utilities)/)(apps),
+    ...group(/\.prefPane$/)(prefs),
+    ...group(/^\/Applications\/Utilities/)(apps),
+    ...group(/System/)(apps),
+    ...group(/Users/)(apps),
+  ].map((value) => {
+    return {
+      name: value.split('/').pop().replace('.app', ''),
+      value,
+      description: value,
+    }
+  })
+}
+
+let app = await arg('Select app:', choices, true)
+let command = `open -a "${app}"`
+if (app.endsWith('.prefPane')) {
+  command = `open ${app}`
+}
+exec(command, {
+  env: {}, //clear the env. Script NODE_PATH/NODE_OPTIONS may conflict.
+})
