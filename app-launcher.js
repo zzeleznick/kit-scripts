21dcfe523b0021afc7e52fd3ce8871af3d718a03
zzeleznick <zzeleznick@gmail.com>
2021-03-26 10:01:20 -0700

    fork app-launcher script to add app images

--- app-launcher.js
+++ app-launcher.js
@@ -1,10 +1,72 @@
 // Menu: App Launcher
 // Description: Search for an app then launch it
-// Author: John Lindquist
-// Twitter: @johnlindquist
+// Author: Zach Zeleznick
+// Twitter: @zzxiv
 // Shortcut: Alt+A
 
-let {fileSearch} = await kit('file')
+const {Icns} = await npm('@fiahfy/icns');
+const {fileSearch} = await kit('file')
+
+// see https://en.wikipedia.org/wiki/Apple_Icon_Image_format#Icon_types
+const formatObjects = [
+  { osType: 'icp6', size: 64, format: 'PNG' },
+  { osType: 'ic07', size: 128, format: 'PNG' },
+  { osType: 'ic08', size: 256, format: 'PNG' },
+  { osType: 'ic09', size: 512, format: 'PNG' },
+]
+
+const formats = formatObjects.map(({osType}) => osType);
+
+const loadIconSet = async (filepath) => {
+  const buf = await readFile(filepath);
+  const icns = Icns.from(buf);
+  const imgBuffers = icns.images.filter(({osType}) => formats.indexOf(osType) !== -1 );
+  // console.log(icns.images[0].osType);
+  imgBuffers.sort((a,b) => a.size > b.size ? -1 : a.size < b.size ? 1 : 0) // reverse sort
+  return imgBuffers.map((icon) => icon.image);
+}
+
+const writeImage = async ({filepath, appName}) => {
+  const imageOutLocation = kenvPath("tmp", "images", appName.replace(".app", ".png"));
+  const imgBuffers = await loadIconSet(filepath);
+  if (!imgBuffers || !imgBuffers.length) {
+    console.warn(`Failed to find images for app ${appName} at ${filepath}`);
+    return
+  }
+  await writeFile(imageOutLocation, imgBuffers[0]);
+}
+
+const createOutDir = () => {
+  const imageDir = kenvPath("tmp", "images");
+  const cmd = `stat -x ${imageDir} > /dev/null 2>&1 || mkdir -p ${imageDir}`
+  exec(cmd, { silent: true });
+}
+
+const fetchIconSets = () => {
+  // NOTE: could do `find /Applications /System/Applications`
+  // but would also miss /System/Library apps and the like
+  const cmd = `find /Applications -name "*.icns" -maxdepth 4 2> /dev/null | sort`;
+  const out = exec(cmd, { silent: true }).toString().split("\n").filter(v => v)
+  let iconSets = out.map((filepath) => {
+    return {
+      filepath,
+      appName: filepath.split('/')[2],
+      iconSetName: filepath.split('/').pop()
+    }
+  });
+  iconSets = iconSets.filter((o, i) => !i || (o.appName !== iconSets[i-1].appName));
+  console.log(JSON.stringify(iconSets, null, 2));
+  return iconSets
+}
+
+const saveIconSets = async () => {
+  createOutDir();
+  const iconSets = fetchIconSets();
+  for (let i = iconSets.length - 1; i >= 0; i--) {
+   await writeImage(iconSets[i]);
+  }
+}
+
 
 let choices = async () => {
   let apps = await fileSearch('', {
@@ -23,6 +85,7 @@ let choices = async () => {
       .sort((a, b) => {
         let aName = a.replace(/.*\//, '')
         let bName = b.replace(/.*\//, '')
+
         return aName > bName ? 1 : aName < bName ? -1 : 0
       })
 
@@ -33,15 +96,27 @@ let choices = async () => {
     ...group(/System/)(apps),
     ...group(/Users/)(apps),
   ].map((value) => {
+    const appName = value.split('/').pop();
+    const img = value.startsWith('/Applications') ? `file://${kenvPath("tmp", "images", appName.replace(".app", ".png"))}` : null;
+    img ? console.log(img) : null;
     return {
-      name: value.split('/').pop().replace('.app', ''),
+      name: appName.replace(/(\.app)$|(\.prefPane)$/, ''),
       value,
       description: value,
+      img,
     }
   })
 }
 
+
+// kit image is broken :/
+// should only display if it exists
+
+// fetchIconSets();
+await saveIconSets();
+
 let app = await arg('Select app:', choices, true)
+
 let command = `open -a "${app}"`
 if (app.endsWith('.prefPane')) {
   command = `open ${app}`
