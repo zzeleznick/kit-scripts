46292609671590112496a6ac201bfe2e1337039a
zzeleznick <zzeleznick@gmail.com>
2021-04-02 17:06:08 -0700

    support loading from remote tree

--- kit-scripts.js
+++ kit-scripts.js
@@ -4,9 +4,6 @@
 // Twitter: @zzxiv
 
 const Prism = await npm('prismjs')
-// Load per suggestion on https://github.com/PrismJS/prism/issues/1171#issuecomment-470929808
-await npm('prismjs/plugins/custom-class/prism-custom-class')
-// injects into Prism.plugins (e.g run 'console.log(Object.keys(Prism.plugins))' before + after)
 
 const scriptsDB = db("kit-scripts", { scripts: [] });
 const scriptsRef = scriptsDB.get("scripts");
@@ -16,6 +13,77 @@ const repo = `scriptkit.app`
 const branch = `main`
 const treepath = `public/scripts/johnlindquist`
 const ref = `${branch}:${treepath}`
+
+const githubURL = "https://api.github.com/graphql";
+
+let token = env.GITHUB_ACCESS_TOKEN;
+
+const config = {
+  headers: {
+    "Authorization": `Bearer ${token}`,
+  }
+}
+
+if (!token) {
+  const element = `
+  <div class="flex flex-col justify-center">
+    <div>
+      <a href="https://github.com/settings/tokens/new">Create a token</a> with "public_repo" enabled.
+    </div>
+    <br>
+    <div>
+      Then, copy + paste the token above or set <code>GITHUB_REPO_TOKEN</code> inside <code>~/.kenv/.env</code>
+    </div>
+  </div>`
+
+  token = await env("GITHUB_ACCESS_TOKEN", {
+    info: `Create and enter your personal access token`,
+    choices: element,
+  });
+}
+
+const repoTreeQuery = `
+query {
+  repository(owner: "${owner}", name: "${repo}") {
+    object(expression: "${ref}") {
+      ... on Tree {
+        entries {
+          name
+        }
+      }
+    }
+  }
+}`
+
+const fetchTreeFiles = async () => {
+  let response;
+  try {
+    response = await post(githubURL,
+      {
+        query: repoTreeQuery
+      },
+      config
+    );
+  }
+  catch (err) {
+    console.warn("fetchTreeFiles failed:", err);
+    return
+  }
+  const graphqlResponse = response.data;
+  console.log(repoTreeQuery, graphqlResponse);
+  const {
+    data: {
+      repository: {
+        object: {
+          entries
+        }
+      }
+    }
+  } = graphqlResponse;
+  const names = entries.map(({name}) => name);
+  console.log("fetchTreeFiles:", names)
+  return names
+}
 
 const fetchScript = async (name) => {
   const scriptUrl = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${treepath}/${name}`;
@@ -58,32 +126,18 @@ const loadScript = async (name) => {
   return payload
 }
 
-// const options = {
-//   name: 'hello-world',
-//   description: 'Enter an name, speak it back',
-//   author: 'John Lindquist',
-//   social: '@johnlindquist',
-// }
-
-// The code snippet you want to highlight, as a string
-// const code = `
-// // Menu: Hello World
-// // Description: Enter an name, speak it back
-// // Author: John Lindquist
-// // Twitter: @johnlindquist
-// let {say} = await kit('speech')
-// let name = await arg("What's your name?")
-// say("Hello, " + "name" + "!")
-// `
-
-
-// see https://github.com/PrismJS/prism/blob/master/plugins/custom-class/prism-custom-class.js
-
-// Prism.plugins.customClass.add(({language, type, content}) => {
-//   if (language === 'javascript') {
-//     return 'overflow-scroll';
-//   }
-// });
+// MARK: currently unused
+const injectCustomClass = async () => {
+  // Load per suggestion on https://github.com/PrismJS/prism/issues/1171#issuecomment-470929808
+  // Source: https://github.com/PrismJS/prism/blob/master/plugins/custom-class/prism-custom-class.js
+  await npm('prismjs/plugins/custom-class/prism-custom-class')
+  // injects into Prism.plugins (e.g run 'console.log(Object.keys(Prism.plugins))' before + after)
+  Prism.plugins.customClass.add(({language, type, content}) => {
+    if (language === 'javascript') {
+      return 'overflow-scroll';
+    }
+  });
+}
 
 const buildCodeBlock = (code) => {
   const html = Prism.highlight(code, Prism.languages.javascript, 'javascript');
@@ -107,22 +161,24 @@ const buildCodeModal = (payload) => {
 
 const injectCss = (html) => {
   // see https://unpkg.com/prism-theme-night-owl@1.4.0/build/light.css
-  // from https://github.com/SaraVieira/prism-theme-night-owl
+  // source: https://github.com/SaraVieira/prism-theme-night-owl
   const css = `code[class*=language-],pre[class*=language-]{color:#403f53;font-family:Consolas,Monaco,"Andale Mono","Ubuntu Mono",monospace;text-align:left;white-space:pre;word-spacing:normal;word-break:normal;word-wrap:normal;line-height:1.5;-moz-tab-size:4;-o-tab-size:4;tab-size:4;-webkit-hyphens:none;-moz-hyphens:none;-ms-hyphens:none;hyphens:none}code[class*=language-] ::-moz-selection,code[class*=language-]::-moz-selection,pre[class*=language-] ::-moz-selection,pre[class*=language-]::-moz-selection{text-shadow:none;background:#fbfbfb}code[class*=language-] ::selection,code[class*=language-]::selection,pre[class*=language-] ::selection,pre[class*=language-]::selection{text-shadow:none;background:#fbfbfb}@media print{code[class*=language-],pre[class*=language-]{text-shadow:none}}pre[class*=language-]{padding:1em;margin:.5em 0;overflow:auto}:not(pre)>code[class*=language-],pre[class*=language-]{color:#fff;background:#fbfbfb}:not(pre)>code[class*=language-]{padding:.1em;border-radius:.3em;white-space:normal}.token.cdata,.token.comment,.token.prolog{color:#989fb1;font-style:italic}.token.punctuation{color:#994cc3}.namespace{color:#0c969b}.token.deleted{color:rgba(239,83,80,.56);font-style:italic}.token.keyword,.token.operator,.token.property,.token.symbol{color:#0c969b}.token.tag{color:#994cc3}.token.boolean{color:#bc5454}.token.number{color:#aa0982}.language-css .token.string,.style .token.string,.token.builtin,.token.char,.token.constant,.token.entity,.token.string,.token.url{color:#4876d6}.token.doctype,.token.function,.token.selector{color:#994cc3;font-style:italic}.token.attr-name,.token.inserted{color:#4876d6;font-style:italic}.token.atrule,.token.attr-value,.token.class-name{color:#111}.token.important,.token.regex,.token.variable{color:#c96765}.token.bold,.token.important{font-weight:700}.token.italic{font-style:italic}`
   const style = `<style type="text/css">${css}</style>`
   return `${style}${html}`
 }
 
 const buildPage = async () => {
-  const payload = await loadScript('anime-search.js');
-  const modals = [buildCodeModal(payload), buildCodeModal(payload)].join('\n');
+  const filenames = await fetchTreeFiles();
+  const promises = filenames.slice(0,3).map(name => loadScript(name));
+  const payloads = await Promise.all(promises);
+  // const payload = await loadScript('anime-search.js');
+  // const modals = [buildCodeModal(payload), buildCodeModal(payload)].join('\n');
+  const modals = payloads.map(buildCodeModal).join('\n');
   const html = `<div style="overflow: hidden;">${modals}</div>`
   const page = injectCss(html)
   console.log(page);
   return page
 }
-
-
 
 const page = await(buildPage);
 
